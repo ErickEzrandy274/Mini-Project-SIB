b7dc8b3bd7fecc1bf8b793aef52379cb86de70c0
Gregory <erick.ezrandy@ui.ac.id>
2023-04-09 11:38:48 +0700

    feat: export all elements

--- components/elements/index.tsx
+++ components/elements/index.tsx
@@ -1,1 +1,8 @@
 export * from "./Shape";
+export * from "./Layout";
+export * from "./Layout/interface";
+export { default as toastOptions } from "./Toaster";
+export { default as ProtectedRoute } from "./ProtectedRoute";
+export * from "./Modal";
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
