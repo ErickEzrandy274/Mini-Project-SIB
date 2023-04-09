e489993fab0e7d584f8448c7598e1694c6a4b4a9
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-04-09 17:17:35 +0700

    feat: setup apollo configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,37 +6,40 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { useMemo } from "react";
 import { useRouter } from "next/router";
 import { ProtectedRoute } from "@elements";
-import { theme, AuthContextProvider } from "@utils";
+import { theme, AuthContextProvider, client } from "@utils";
+import { ApolloProvider } from "@apollo/client";
 
 export default function App({ Component, pageProps }: AppProps) {
 	const { pathname } = useRouter();
 	const noAuthURL = useMemo(() => ["/", "/jobs"], []);
 
 	return (
-		<AuthContextProvider>
-			<ChakraProvider theme={theme}>
-				<Head>
-					<title>Job Listing Vacancies</title>
-					<meta name="description" content="Job Listing Vacancies" />
-					<link rel="icon" href="/job-vacancy-logo.png" />
-				</Head>
+		<ApolloProvider client={client}>
+			<AuthContextProvider>
+				<ChakraProvider theme={theme}>
+					<Head>
+						<title>Job Listing Vacancies</title>
+						<meta name="description" content="Job Listing Vacancies" />
+						<link rel="icon" href="/job-vacancy-logo.png" />
+					</Head>
 
-				{noAuthURL.includes(pathname) ? (
-					pathname === "/" ? (
-						<Component {...pageProps} />
+					{noAuthURL.includes(pathname) ? (
+						pathname === "/" ? (
+							<Component {...pageProps} />
+						) : (
+							<Layout>
+								<Component {...pageProps} />
+							</Layout>
+						)
 					) : (
 						<Layout>
-							<Component {...pageProps} />
+							<ProtectedRoute>
+								<Component {...pageProps} />
+							</ProtectedRoute>
 						</Layout>
-					)
-				) : (
-					<Layout>
-						<ProtectedRoute>
-							<Component {...pageProps} />
-						</ProtectedRoute>
-					</Layout>
-				)}
-			</ChakraProvider>
-		</AuthContextProvider>
+					)}
+				</ChakraProvider>
+			</AuthContextProvider>
+		</ApolloProvider>
 	);
 }
