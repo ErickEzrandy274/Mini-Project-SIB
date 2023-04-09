ae176435398f20e5f3c1fc974ff9c7afa5af8fce
Gregory <erick.ezrandy@ui.ac.id>
2023-04-09 11:38:31 +0700

    feat: create protected route component

---
+++ components/elements/ProtectedRoute/index.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { LayoutProps, LoginModal } from "@elements";
+import { useAuth } from "@context";
+import { useDisclosure } from "@chakra-ui/react";
+
+const ProtectedRoute: React.FC<LayoutProps> = ({ children }) => {
+	const { isOpen, onClose, onOpen } = useDisclosure();
+	const { user } = useAuth();
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	useEffect(() => {
+		if (!user) {
+			onOpen();
+			return;
+		}
+
+		setIsAuthenticated(true);
+	}, [user, onOpen]);
+
+	if (!isAuthenticated) {
+		return <LoginModal isOpen={isOpen} onClose={onClose} />;
+	}
+
+	return <>{children}</>;
+};
+
+export default ProtectedRoute;
