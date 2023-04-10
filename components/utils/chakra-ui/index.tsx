7ed776f0b9a431ec86b6a70e0c22efbb55670b69
Gregory <erick.ezrandy@ui.ac.id>
2023-04-10 11:51:34 +0700

    chore: extend chakra ui theme

--- components/utils/chakra-ui/index.tsx
+++ components/utils/chakra-ui/index.tsx
@@ -1,8 +1,42 @@
 import { extendTheme } from "@chakra-ui/react";
 
+const field = {
+	backgroundColor: "gray.300",
+	border: "none",
+	boxShadow: "md",
+	_hover: {
+		backgroundColor: "gray.400",
+	},
+	_focus: {
+		backgroundColor: "gray.400",
+		boxShadow: "none",
+	},
+};
+
 const theme = extendTheme({
 	fonts: {
 		body: "'Poppins', sans-serif",
+	},
+	components: {
+		Input: {
+			variants: {
+				filled: {
+					field,
+				},
+			},
+		},
+		Select: {
+			variants: {
+				filled: {
+					field,
+				},
+			},
+		},
+		Textarea: {
+			variants: {
+				filled: field,
+			},
+		},
 	},
 });
 
--- components/utils/index.tsx
+++ components/utils/index.tsx
@@ -2,3 +2,4 @@ export * from "./firebase";
 export { default as theme } from "./chakra-ui";
 export * from "./context";
 export * from "./graphql";
+export * from "./helper";
