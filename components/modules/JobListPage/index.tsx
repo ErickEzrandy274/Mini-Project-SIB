029f3eb9dc29ff1458f953b834e1947e1c744189
Gregory <erick.ezrandy@ui.ac.id>
2023-04-11 16:55:35 +0700

    feat: create job pages

---
+++ components/modules/JobListPage/index.tsx
@@ -0,0 +1,49 @@
+import { useSubscription } from "@apollo/client";
+import { Flex, Heading, Text } from "@chakra-ui/react";
+import { PaginatedItems, PrimaryLoading } from "@elements";
+import { JOB_VACANCIES_SUBSCRIPTION } from "@utils";
+import React from "react";
+
+const JobListPage = () => {
+	const { data, loading } = useSubscription(JOB_VACANCIES_SUBSCRIPTION);
+
+	return (
+		<Flex flexDirection="column" align="center" justify="center" gap={5} p={5}>
+			<Heading
+				as="h2"
+				fontSize={{ base: "3xl", md: "4xl", xl: "5xl" }}
+				textAlign="center"
+				bgGradient="linear(to-br, messenger.500, facebook.700)"
+				bgClip="text"
+			>
+				List of Job Vacancy
+			</Heading>
+
+			{loading ? (
+				<PrimaryLoading />
+			) : !!data.job_vacancy.length ? (
+				<PaginatedItems itemsPerPage={4} items={data.job_vacancy} />
+			) : (
+				<Flex flexDirection="column" textAlign="center" gap={2}>
+					<Heading
+						as="h3"
+						color="gray.600"
+						fontSize={{ base: "xl", md: "2xl", xl: "3xl" }}
+					>
+						There are currently no job vacancies
+					</Heading>
+
+					<Text
+						fontWeight="medium"
+						color="gray.500"
+						fontSize={{ base: "md", md: "lg", xl: "xl" }}
+					>
+						please check our website regularly!
+					</Text>
+				</Flex>
+			)}
+		</Flex>
+	);
+};
+
+export default JobListPage;
--- components/modules/index.tsx
+++ components/modules/index.tsx
@@ -1,2 +1,3 @@
 export { default as LandingPage } from "./LandingPage";
 export { default as CreateJobPage } from "./CreateJobPage";
+export { default as JobListPage } from "./JobListPage";
