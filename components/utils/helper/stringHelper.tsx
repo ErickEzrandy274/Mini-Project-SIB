1c9d67c4d9e3d167e8d7fa71dd5e7e3b2f11997d
Gregory <erick.ezrandy@ui.ac.id>
2023-04-13 13:41:20 +0700

    refactor: remove toCapitalize method and add encode decode method

--- components/utils/helper/stringHelper.tsx
+++ components/utils/helper/stringHelper.tsx
@@ -1,8 +1,21 @@
-export const toCapitalize = (input: string) => {
-	return input.at(0)?.toUpperCase() + input.slice(1);
-};
-
 export const splitText = (input: string, separator: string) => {
 	const rawInput = input.split(separator);
 	return rawInput.join(" ");
+};
+
+export const encode = (decoded: string) => {
+	return window.btoa(encodeURIComponent(decoded));
+};
+
+export const decode = (encoded: string) => {
+	return decodeURIComponent(window.atob(encoded));
+};
+
+export const dateFormat = (date: string) => {
+	return Intl.DateTimeFormat("en-GB", {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	}).format(new Date(date));
 };
