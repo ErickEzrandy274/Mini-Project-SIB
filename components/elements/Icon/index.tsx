1069fd1480828c2c177b8939741b2cfd289fa7ca
Gregory <erick.ezrandy@ui.ac.id>
2023-04-13 14:21:38 +0700

    feat: create salary icon

--- components/elements/Icon/index.tsx
+++ components/elements/Icon/index.tsx
@@ -1,2 +1,4 @@
 export { default as LocationIcon } from "./LocationIcon";
 export { default as TimeIcon } from "./TimeIcon";
+export { default as SalaryIcon } from "./SalaryIcon";
+export { default as BriefcaseIcon } from "./BriefcaseIcon";
