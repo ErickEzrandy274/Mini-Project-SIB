38231e215e62aa6b494d6aef6849bad86ff5d045
Gregory <erick.ezrandy@ui.ac.id>
2023-04-15 13:57:52 +0700

    chore: export all icon components

--- components/elements/Icon/index.tsx
+++ components/elements/Icon/index.tsx
@@ -2,3 +2,5 @@ export { default as LocationIcon } from "./LocationIcon";
 export { default as TimeIcon } from "./TimeIcon";
 export { default as SalaryIcon } from "./SalaryIcon";
 export { default as BriefcaseIcon } from "./BriefcaseIcon";
+export { default as UploadIcon } from "./UploadIcon";
+export { default as PDFIcon } from "./PDFIcon";
