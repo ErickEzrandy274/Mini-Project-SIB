3d3007eeb38faec368f2a61afcfe8b3236790993
Gregory <erick.ezrandy@ui.ac.id>
2023-04-16 11:43:20 +0700

    refactor: use fragments to avoid duplicate code

--- components/utils/graphql/subscriptions.ts
+++ components/utils/graphql/subscriptions.ts
@@ -1,4 +1,5 @@
 import { gql } from "@apollo/client";
+import { JOB_VACANCY_FIELDS_FRAGMENT } from "./fragments";
 
 export const JOB_VACANCIES_SUBSCRIPTION = gql`
 	subscription JobVacanciesSubscription($uid: String!) {
@@ -6,14 +7,10 @@ export const JOB_VACANCIES_SUBSCRIPTION = gql`
 			where: { user: { id: { _neq: $uid } } }
 			order_by: { name: asc }
 		) {
-			id
-			location
-			name
-			company_name
-			created_at
-			edited_at
+			...JobVacancyFields
 		}
 	}
+	${JOB_VACANCY_FIELDS_FRAGMENT}
 `;
 
 export const JOB_VACANCIES_SUBSCRIPTION_OWNED_BY_CURRENT_USER = gql`
@@ -22,25 +19,40 @@ export const JOB_VACANCIES_SUBSCRIPTION_OWNED_BY_CURRENT_USER = gql`
 			where: { user: { id: { _eq: $uid } } }
 			order_by: { name: asc }
 		) {
-			id
-			location
-			name
-			company_name
-			created_at
-			edited_at
+			...JobVacancyFields
 		}
 	}
+	${JOB_VACANCY_FIELDS_FRAGMENT}
 `;
 
 export const JOB_VACANCIES_SUBSCRIPTION_APPLIED_BY_CURRENT_USER = gql`
 	subscription MySubscription($uid: String!) {
 		job_vacancy(where: { applicants: { userId: { _eq: $uid } } }) {
-			id
-			location
-			name
+			...JobVacancyFields
+		}
+	}
+	${JOB_VACANCY_FIELDS_FRAGMENT}
+`;
+
+export const SUBSCRIPTION_JOB_BY_ID = gql`
+	subscription SubscriptionJobById($id: uuid!) {
+		job_vacancy_by_pk(id: $id) {
 			company_name
 			created_at
 			edited_at
+			description
+			salary
+			name
+			location
+			id
+			working_type
+			user {
+				id
+				name
+			}
+			applicants {
+				userId
+			}
 		}
 	}
 `;
