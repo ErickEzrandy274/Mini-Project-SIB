fe989374e5ad056dcb24ef370cd45f4659bc6b8b
Gregory <erick.ezrandy@ui.ac.id>
2023-04-18 18:02:31 +0700

    feat: create functionality of update user's state

--- components/elements/Card/ApplicantCard.tsx
+++ components/elements/Card/ApplicantCard.tsx
@@ -1,55 +1,153 @@
-import React from "react";
-import { Button, Card, CardBody, Flex, Text } from "@chakra-ui/react";
+import React, { BaseSyntheticEvent, useState } from "react";
+import {
+	Button,
+	Card,
+	CardBody,
+	Flex,
+	FormControl,
+	Select,
+	Text,
+} from "@chakra-ui/react";
 import { ApplicantCardProps } from "./interface";
 import { StatusBadge } from "@elements";
 import Link from "next/link";
+import { statusOptions } from "./constant";
+import { useMutation } from "@apollo/client";
+import { UPDATE_STATUS_APPLICANT } from "@utils";
+import { toast } from "react-hot-toast";
 
 const ApplicantCard: React.FC<ApplicantCardProps> = ({
+	id,
 	status,
 	name,
 	link_url,
 	userId,
 }) => {
+	const [newStatus, setNewStatus] = useState<
+		"none" | "applied" | "under review" | "interviewing"
+	>(status);
+	const [isUpdate, setIsUpdate] = useState<boolean>(false);
+	const [updateApplicantState] = useMutation(UPDATE_STATUS_APPLICANT);
+
+	const handleChange = (e: BaseSyntheticEvent) => {
+		setNewStatus(e.target.value);
+	};
+
+	const handleSubmit = async (e: BaseSyntheticEvent) => {
+		e.preventDefault();
+		toast.loading("Updating applicant state...");
+		if (newStatus === status) {
+			toast.dismiss();
+			setIsUpdate(false);
+			return toast("Nothing has changed!");
+		}
+
+		try {
+			await updateApplicantState({ variables: { id, status: newStatus } });
+			toast.dismiss();
+			toast.success(`Successfully update ${name}'s application status`);
+		} catch (error: any) {
+			toast.dismiss();
+			toast.error(error.message);
+		}
+		setIsUpdate(false);
+	};
+
 	return (
 		<Card
 			bgGradient="linear(to-b, #334155, #1f2937)"
 			color="gray.200"
 			rounded="xl"
 		>
-			<CardBody
-				display="flex"
-				flexDirection="column"
-				gap={2}
-				p={{ base: 3, md: 4 }}
-			>
-				<Flex justifyContent="space-between" alignItems="center" gap={3}>
-					<Text fontSize={{ base: "sm", md: "md" }}>{name}</Text>
+			<form onSubmit={handleSubmit}>
+				<CardBody
+					display="flex"
+					flexDirection="column"
+					gap={2}
+					p={{ base: 3, md: 4 }}
+				>
+					<Flex justifyContent="space-between" alignItems="center" gap={3}>
+						<Text fontSize={{ base: "sm", md: "md" }}>{name}</Text>
 
-					<Button
-						colorScheme="twitter"
-						letterSpacing="0.2px"
-						size="sm"
-						rounded="md"
+						<Button
+							colorScheme="twitter"
+							letterSpacing="0.2px"
+							size="sm"
+							rounded="md"
+						>
+							<Link href={link_url} target="_blank">
+								Resume
+							</Link>
+						</Button>
+					</Flex>
+
+					<Flex
+						flexDirection={{ base: "column", md: "row" }}
+						justifyContent="space-between"
+						alignItems={{
+							base: isUpdate ? "flex-end" : "flex-start",
+							md: "center",
+						}}
+						gap={2}
 					>
-						<Link href={link_url} target="_blank">
-							Resume
-						</Link>
-					</Button>
-				</Flex>
+						{isUpdate ? (
+							<FormControl>
+								<Select
+									variant="filled"
+									id="new_status"
+									name="new_status"
+									value={newStatus}
+									color="black"
+									onChange={handleChange}
+									w={{ base: "full", md: "90%" }}
+								>
+									{statusOptions.map((value) => {
+										return (
+											<option key={value} value={value}>
+												{value}
+											</option>
+										);
+									})}
+								</Select>
+							</FormControl>
+						) : (
+							<StatusBadge status={status} />
+						)}
 
-				<Flex justifyContent="space-between" alignItems="center">
-					<StatusBadge status={status} />
+						{isUpdate ? (
+							<Flex gap={{ base: 1, md: 2 }}>
+								<Button
+									colorScheme="red"
+									size="sm"
+									rounded="md"
+									onClick={() => setIsUpdate(false)}
+								>
+									Cancel
+								</Button>
 
-					<Button
-						colorScheme="facebook"
-						letterSpacing="0.2px"
-						size="sm"
-						rounded="md"
-					>
-						Update
-					</Button>
-				</Flex>
-			</CardBody>
+								<Button
+									type="submit"
+									colorScheme="green"
+									size="sm"
+									rounded="md"
+								>
+									Save
+								</Button>
+							</Flex>
+						) : (
+							<Button
+								colorScheme="facebook"
+								letterSpacing="0.2px"
+								size="sm"
+								rounded="md"
+								onClick={() => setIsUpdate(true)}
+							>
+								Update State
+							</Button>
+						)}
+					</Flex>
+				</CardBody>
+			</form>
 		</Card>
 	);
 };
