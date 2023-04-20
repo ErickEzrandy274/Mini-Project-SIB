31e367b2f6a4a29718cccd36e40ebe4ce8115f7d
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-04-20 20:02:25 +0700

    feat: define query that is owned or apllied by current user

--- components/utils/graphql/queries.ts
+++ components/utils/graphql/queries.ts
@@ -1,4 +1,5 @@
 import { gql } from "@apollo/client";
+import { JOB_VACANCY_FIELDS_FRAGMENT } from "./fragments";
 
 export const GET_USER_BY_ID = gql`
 	query GetUserById($id: String!) {
@@ -35,4 +36,37 @@ export const GET_JOB_BY_ID = gql`
 			}
 		}
 	}
+`;
+
+export const JOB_VACANCIES_QUERY = gql`
+	query JobVacanciesQuery($uid: String!) {
+		job_vacancy(
+			where: { user: { id: { _neq: $uid } } }
+			order_by: { name: asc }
+		) {
+			...JobVacancyFields
+		}
+	}
+	${JOB_VACANCY_FIELDS_FRAGMENT}
+`;
+
+export const JOB_VACANCIES_QUERY_OWNED_BY_CURRENT_USER = gql`
+	query JobVacanciesQuery($uid: String!) {
+		job_vacancy(
+			where: { user: { id: { _eq: $uid } } }
+			order_by: { name: asc }
+		) {
+			...JobVacancyFields
+		}
+	}
+	${JOB_VACANCY_FIELDS_FRAGMENT}
+`;
+
+export const JOB_VACANCIES_QUERY_APPLIED_BY_CURRENT_USER = gql`
+	query JobVacanciesQuery($uid: String!) {
+		job_vacancy(where: { applicants: { userId: { _eq: $uid } } }) {
+			...JobVacancyFields
+		}
+	}
+	${JOB_VACANCY_FIELDS_FRAGMENT}
 `;
