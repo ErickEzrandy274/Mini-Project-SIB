7c15ddcc42880490427e5d7a455bf56a4ff939bf
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-04-24 14:12:24 +0700

    feat: implement animation on landing page

--- components/modules/LandingPage/index.tsx
+++ components/modules/LandingPage/index.tsx
@@ -2,6 +2,8 @@ import React, { useMemo } from "react";
 import { Button, Flex, Heading, Text } from "@chakra-ui/react";
 import { Circle, Square } from "@elements";
 import { LandingPageStyle } from "./constant";
+import { motion } from "framer-motion";
+import { opScaleAnimations } from "@utils";
 import Router from "next/router";
 
 const LandingPage = () => {
@@ -16,6 +18,8 @@ const LandingPage = () => {
 		secondBoxTop,
 		secondBoxRight,
 	} = useMemo(() => LandingPageStyle, []);
+
+	const { initial, animate } = useMemo(() => opScaleAnimations, []);
 
 	return (
 		<Flex
@@ -33,6 +37,7 @@ const LandingPage = () => {
 					height={squareSize}
 					width={squareSize}
 					rotate={150}
+					delay={0.1}
 				/>
 
 				<Flex
@@ -51,29 +56,48 @@ const LandingPage = () => {
 					backdropFilter="blur(5px)"
 				>
 					<Heading
-						as="h1"
+						as={motion.h1}
 						size={{ base: "xl", md: "2xl" }}
 						bgGradient="linear-gradient(to-r, #9333ea, #6d28d9, #FF0080)"
 						bgClip="text"
+						initial={initial}
+						animate={animate}
+						transition="1s ease-out 0.2s"
 					>
 						Find Your Next Dream Job
 					</Heading>
 
 					<Flex flexDirection="column" gap={gapText} fontWeight="medium">
-						<Text fontSize={{ base: "md", md: "xl" }}>
+						<Text
+							as={motion.p}
+							fontSize={{ base: "md", md: "xl" }}
+							initial={initial}
+							animate={animate}
+							transition="1s ease-out 0.3s"
+						>
 							We help you discover exciting opportunities around the world
 						</Text>
 
-						<Text fontSize={{ base: "md", md: "xl" }}>
+						<Text
+							as={motion.p}
+							fontSize={{ base: "md", md: "xl" }}
+							initial={initial}
+							animate={animate}
+							transition="1s ease-out 0.4s"
+						>
 							Make sure you know the latest jobs that suit you
 						</Text>
 					</Flex>
 
 					<Button
-						p={{ base: "4", md: "6" }}
+						as={motion.button}
+						p={{ base: "4", md: "5" }}
 						colorScheme="red"
 						borderRadius="xl"
 						onClick={() => Router.push("/jobs")}
+						initial={initial}
+						animate={animate}
+						transition="ease-in-out 0.8s"
 					>
 						Find Job Now
 					</Button>
@@ -85,6 +109,7 @@ const LandingPage = () => {
 					height={squareSize}
 					width={squareSize}
 					rotate={45}
+					delay={0.15}
 				/>
 
 				<Circle
@@ -92,6 +117,7 @@ const LandingPage = () => {
 					right={circleRight}
 					height={120}
 					width={120}
+					delay={0.2}
 				/>
 			</Flex>
 		</Flex>
