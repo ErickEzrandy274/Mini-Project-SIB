5f215c95196cd9271b34adbf0633c08e52ca6790
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-05-11 08:40:56 +0700

    chore: use several poppins's weight

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
 		<Html lang="en">
 			<Head>
 				<link
-					href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
 					rel="stylesheet"
 				/>
 			</Head>
