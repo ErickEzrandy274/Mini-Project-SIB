117285f37cdcba9b3308ade70b8b2b8bc6a7933a
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2024-07-19 09:34:12 +0700

    chore: create constant object

---
+++ components/elements/Paginated/constant.ts
@@ -0,0 +1,10 @@
+interface OptionProps {
+	value: number;
+	text: string;
+}
+
+export const options: OptionProps[] = [
+	{ value: 5, text: "5 items/page" },
+	{ value: 10, text: "10 items/page" },
+	{ value: 15, text: "15 items/page" },
+];
